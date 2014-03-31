54c82a08b14a130d2c7b3a2dc1cd9e00b2a7399e
lutfi <artbomberlutfi@gmail.com>
2014-03-31 22:02:02 +0700

    Create belajar-json.php
    contoh pertama nih der

---
+++ belajar-json.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<title>JSON </title>
+<script type='text/javascript'>
+	var profile = {nama:’Lutfi Fars Hidayat’};
+	document.write (profile.nama);
+</script>
+</head>
+<body>
+</body>
+</html>
